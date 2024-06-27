a8b8960d84c398bc507438006c5f554d8e668cf6
Charles <Charleskojomark>
2024-06-27 12:10:14 +0100

    db

--- ojm_script/settings.py
+++ ojm_script/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 import dj_database_url
+import pymysql
 from pathlib import Path
 from dotenv import load_dotenv
+
+
+pymysql.install_as_MySQLdb()
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
